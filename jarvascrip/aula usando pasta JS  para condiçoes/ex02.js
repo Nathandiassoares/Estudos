cefc499ff5302adef75b3c1169528414396bcb4a
Nathandisk <103308730+Nathandiassoares@users.noreply.github.com>
2022-07-14 21:47:33 -0300

    level up no form/execicios no JS

---
+++ jarvascrip/aula usando pasta JS  para condiçoes/ex02.js
@@ -0,0 +1,6 @@
+var lol = 'bronze'
+if(lol=='platina'){
+    console.log (`Você e ${lol} então joga bem!`)
+} else {
+    console.log(`Você e ${lol} então joga mal!`)
+}
---
+++ jarvascrip/aula usando pasta JS  para condiçoes/exo1.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do meu carro esta a ${vel}Km/h`)
+if(vel > 60) { //condição simplis....
+    console.log(`Você ultrapassou a velocidade pertimida,MULTADO!`)
+}
+console.log(`Digira sempre com cinto de segurança`)
